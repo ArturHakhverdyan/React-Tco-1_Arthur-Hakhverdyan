406e76a4577d1d0255a79b91934ea01f411b2493
ArturHakhverdyan <arthurhakhverdyan1@gmail.com>
2022-04-29 17:53:35 +0300

    Lesson-4 React-JS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-
-import { DIV1 } from "./DIV1/DIV1"
-import {DIV} from "./DIV/DIV"
-import { TOP_BLOCK } from "./TOP_BlOCK/TOP_BLOCK";
-import { MID } from "./MIDDLE_BLOCK/MIDDLE_BLOCK";
-import { HeadBlock } from "./Header_block";
-import { ImgWrap } from "./Img_Block";
- 
-
+import { FacebookList } from "./FacebookList";
 
 function App() {
   return (
-    <div className="App" id="barev">
-        <DIV1 />
-        <DIV />
-        <TOP_BLOCK />
-        <MID />
-        <HeadBlock />
-        <ImgWrap />
+    <div className="App" >
+       <FacebookList />
     </div>
   );
 }
