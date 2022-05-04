5952b3fc05e3e84abefd6e52c1a733462358325b
ArturHakhverdyan <arthurhakhverdyan1@gmail.com>
2022-05-04 14:09:19 +0300

    deleted-naster

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 
-import { DIV1 } from "./DIV1/DIV1"
-import {DIV} from "./DIV/DIV"
-import { TOP_BLOCK } from "./TOP_BlOCK/TOP_BLOCK";
-import { MID } from "./MIDDLE_BLOCK/MIDDLE_BLOCK";
-import { HeadBlock } from "./Header_block";
-import { ImgWrap } from "./Img_Block";
- 
-
-
 function App() {
   return (
     <div className="App" id="barev">
-        <DIV1 />
-        <DIV />
-        <TOP_BLOCK />
-        <MID />
-        <HeadBlock />
-        <ImgWrap />
+     
     </div>
   );
 }
--- src/Img_Block/Imges/index.jsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-export function Imges (props) {
-    return(
-        <div className='imges'>
-            <img src={props.ImgUrl} alt="" className='imges-img'/>
-        </div>
-    )
-}
